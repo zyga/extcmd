060b9e59678ea3d79d1c3f9edd89d163cdabfed2
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2012-11-26 13:57:09 +0100

    Mark python3.2 and python3.3 as supported
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- setup.py
+++ setup.py
@@ -38,5 +38,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
     ],
     zip_safe=True)
